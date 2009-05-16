04a640979bf48cc0cf5e1bd0bea9e004743611f6
Arthur Taylor <codders@octomonkey.org.uk>
2009-05-16 23:21:05 +0100

    Made image parsing more verbose

--- bundles/exif/src/main/java/uk/co/talkingcode/ipojorc/commands/exif/Exif.java
+++ bundles/exif/src/main/java/uk/co/talkingcode/ipojorc/commands/exif/Exif.java
@@ -5,6 +5,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import org.apache.sanselan.ImageInfo;
 import org.apache.sanselan.ImageReadException;
 import org.apache.sanselan.Sanselan;
 import org.apache.sanselan.common.IImageMetadata;
@@ -29,14 +30,46 @@ public class Exif extends AbstractURLWatchingPrefixCommand {
       url = lastUrl;
     }
     if (url != null && url.length() != 0) {
-      try {
-        return message.createReply(getExif(url));
-      } catch (Exception e) {
-        System.err.println("Unable to process data for " + lastUrl);
-        e.printStackTrace();
-      }
+      return message.createReply(getImageDetails(url));
     }
     return null;
+  }
+  
+  private String getImageDetails(String url)
+  {
+    try {
+      return getExif(url);
+    } catch (Exception e) {
+      System.err.println("Unable to process data for " + url);
+      e.printStackTrace();
+    }
+    try
+    {
+      return getImageInfo(url);
+    }
+    catch (Exception e)
+    {
+      System.err.println("Unable to process data for " + url);
+    }
+    return "Unable to load image: " + url;
+  }
+
+  @SuppressWarnings("unchecked")
+  private String getImageInfo(String url) throws ImageReadException, MalformedURLException, IOException {
+    ImageInfo info = Sanselan.getImageInfo(new URL(url).openStream(), null);
+
+    String result = "Image " + info.getHeight() + " x " + info.getWidth() + " x " + info.getBitsPerPixel();
+    List comments = info.getComments();
+    if (comments != null && comments.size() != 0)
+    {
+      StringBuilder commentString = new StringBuilder("");
+      for (Object comment : comments)
+      {
+        commentString.append(", " + comment);
+      }
+      result += ", Comments: " + commentString.toString();
+    }
+    return result;
   }
 
   @SuppressWarnings("unchecked")
