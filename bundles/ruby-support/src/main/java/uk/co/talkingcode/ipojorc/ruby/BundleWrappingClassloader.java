a72875d3b855b1d3b37924d5b276e033f546e3a8
Arthur Taylor <codders@octomonkey.org.uk>
2009-06-06 17:59:58 +0100

    Tidy up

--- bundles/ruby-support/src/main/java/uk/co/talkingcode/ipojorc/ruby/BundleWrappingClassloader.java
+++ bundles/ruby-support/src/main/java/uk/co/talkingcode/ipojorc/ruby/BundleWrappingClassloader.java
@@ -1,9 +1,6 @@
 package uk.co.talkingcode.ipojorc.ruby;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.util.Enumeration;
 
 import org.osgi.framework.Bundle;
 
@@ -17,48 +14,20 @@ public class BundleWrappingClassloader extends ClassLoader {
     this.bundle = bundle;
   }
 
-  @SuppressWarnings("unchecked")
-  public Class loadClass(String name) throws ClassNotFoundException {
-    System.out.println("Bundle loading class: " + name);
-    return bundle.loadClass(name);
-  }
-
   @Override
   public URL getResource(String name) {
-    System.out.println("Bundle Loading Resource: " + name);
     return bundle.getResource(name);
   }
 
   @Override
-  public InputStream getResourceAsStream(String name) {
-    System.out.println("Bundle Loading Resource as stream: " + name);
-    try {
-      return bundle.getResource(name).openStream();
-    } catch (IOException e) {
-      System.err.println("Unable to load resource " + name + " as stream: " + e.getMessage());
-      e.printStackTrace();
-    }
-    return null;
-  }
-
-  @SuppressWarnings("unchecked")
-  @Override
-  public Enumeration<URL> getResources(String name) throws IOException {
-    System.out.println("Bundle Loading Resources: " + name);
-    return bundle.getResources(name);
-  }
-
-  @Override
   protected Class<?> findClass(String name) throws ClassNotFoundException {
-    System.out.println("Bundle finding class: " + name);
     try
     {
-      System.out.println("Attempting to load from bundle");
-      return loadClass(name);
+      return bundle.loadClass(name);
     }
     catch (Exception e)
     {
-      System.err.println("Unable to load " + name + " from bundle. Searching system");
+      System.err.println("Unable to load " + name + " from bundle. Searching default classpath");
     }
     return super.findClass(name);
   }
--- bundles/ruby-support/src/main/java/uk/co/talkingcode/ipojorc/ruby/ExampleActivator.java
+++ bundles/ruby-support/src/main/java/uk/co/talkingcode/ipojorc/ruby/ExampleActivator.java
@@ -2,7 +2,6 @@ package uk.co.talkingcode.ipojorc.ruby;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.jruby.Ruby;
@@ -27,23 +26,17 @@ public class ExampleActivator implements BundleActivator, BundleListener {
   public void start(BundleContext bc) throws Exception {
     bc.addBundleListener(this);
     context = bc;
-    List<String> loadPaths = new ArrayList<String>();
-    //loadPaths.add("jruby-complete-1.3.0.jar");
     RubyInstanceConfig config = new RubyInstanceConfig();
     config.setLoader(new BundleWrappingClassloader(Thread.currentThread()
         .getContextClassLoader(), bc.getBundle()));
     runtime = Ruby.newInstance(config);
-    runtime.getLoadService().init(loadPaths);
+    runtime.getLoadService().init(new ArrayList<String>());
   }
 
   private void evalLoadedScript(String script, Bundle bundle)
       throws IOException {
-    ClassLoader loader = Thread.currentThread().getContextClassLoader();
-    ClassLoader wrapper = new BundleWrappingClassloader(loader, context.getBundle());
-    Thread.currentThread().setContextClassLoader(wrapper);
     runtime.evalScriptlet(IOUtils.toString(bundle.getResource(script)
         .openStream()));
-    Thread.currentThread().setContextClassLoader(loader);
   }
 
   /**
